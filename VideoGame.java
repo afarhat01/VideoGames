965d49215600845a06aa8c487018870504d65949
Student: Alex Alex <94848745+afarhat01@users.noreply.github.com>
2021-11-30 14:44:34 +0000

    Added mutator methods

--- VideoGame.java
+++ VideoGame.java
@@ -1,18 +1,41 @@
 class VideoGame
 {
 private String name;
-private String genre;
+private String company;
 private int year;
 private boolean console;
 
+public VideoGame(String n, String c, int y, boolean con)
+{
+  setName(n);
+  setCompany(c);
+  setYear(y);
+  setConsole(con);
+}
 
 
 
 
 
 
+public void setName(String n)
+{
+  name = n;
+}
 
+public void setCompany(String c)
+{
+  company = c;
+}
 
+public void setYear(int y)
+{
+  year = y;
+}
 
+public void setConsole(boolean con)
+{
+  console = con;
+}
 
 }
